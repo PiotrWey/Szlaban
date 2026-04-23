1f1ef43fde39cc7b9f80992cfd466ecad81621d2
Piotr W. <git@piotr-weychan.dev>
2026-04-23 16:34:09 +0100

    update build system versions, switch to paper devkit
    - updated gradle, run paper
    - removed warning suppression

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,11 @@
-@file:Suppress("UNRESOLVED_REFERENCE")
-// IntelliJ has a problem where it screams at me when I try to use the
-// ShadowJar plugin in Gradle Kotlin DSL. This gets rid of 2/3 of associated
-// issues.
-
 import xyz.jpenilla.runpaper.task.RunServer
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    java
-    id("xyz.jpenilla.run-paper") version "2.3.1"
-    id("com.gradleup.shadow") version "9.4.1"
+  java
+  id("xyz.jpenilla.run-paper") version "3.0.2"
+  id("com.gradleup.shadow") version "9.4.1"
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
 }
 
 group = project.property("group") as String
@@ -19,102 +15,106 @@ version = project.property("version") as String
 val targetMcVersion = project.property("target") as String
 
 repositories {
-    mavenCentral()
-    maven {
-        name = "papermc-repo"
-        url = uri("https://repo.papermc.io/repository/maven-public/")
-    }
+  mavenCentral()
+  maven {
+    name = "papermc-repo"
+    url = uri("https://repo.papermc.io/repository/maven-public/")
+  }
 }
 
 dependencies {
-    // compile-against APIs
-    compileOnly("io.papermc.paper:paper-api:${targetMcVersion}-R0.1-SNAPSHOT")
-    compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
+  // Paper dev bundle
+  paperweight.paperDevBundle("${targetMcVersion}-R0.1-SNAPSHOT")
 
-    // unit tests
-    testImplementation(platform("org.junit:junit-bom:6.0.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    // mocking framework
-    testImplementation("org.mockito:mockito-core:5.+")
-    // libraries for tests
-    testImplementation("io.papermc.paper:paper-api:${targetMcVersion}-R0.1-SNAPSHOT")
+  // compile-against APIs
+  // compileOnly("io.papermc.paper:paper-api:${targetMcVersion}-R0.1-SNAPSHOT")
+  compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
 
-
-    tasks {
-        // run a server with the recommended dependencies
-        named<RunServer>("runServer") {
-            minecraftVersion(targetMcVersion)
-            downloadPlugins {
-                github("dmulloy2", "ProtocolLib", "5.4.0", "ProtocolLib.jar")
-            }
-        }
-
-        jar {
-            manifest {
-                attributes(
-                    "paperweight-mappings-namespace" to "mojang"
-                )
-            }
-        }
-
-        // Non-standard syntax. Due to bugs with how IntelliJ loads the ShadowJar plugin
-        // This should be replaced with the following line if it is fixed
-        // shadowJar {
-        named<ShadowJar>("shadowJar", ShadowJar::class) {
-            manifest {
-                attributes(
-                    "paperweight-mappings-namespace" to "mojang"
-                )
-            }
-        }
-
-        test {
-            useJUnitPlatform()
-            jvmArgs(
-                "-XX:+EnableDynamicAgentLoading"
-            )
-        }
-    }
+  // unit tests
+  testImplementation(platform("org.junit:junit-bom:6.0.3"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  // mocking framework
+  testImplementation("org.mockito:mockito-core:5.+")
+  // libraries for tests
+  // testImplementation("io.papermc.paper:paper-api:${targetMcVersion}-R0.1-SNAPSHOT")
 }
+
+tasks {
+  // run a server with the recommended dependencies
+  named<RunServer>("runServer") {
+    minecraftVersion(targetMcVersion)
+    downloadPlugins {
+      github("dmulloy2", "ProtocolLib", "5.4.0", "ProtocolLib.jar")
+    }
+  }
+
+  jar {
+    manifest {
+      attributes(
+        "paperweight-mappings-namespace" to "mojang"
+      )
+    }
+  }
+
+  // Non-standard syntax. Due to bugs with how IntelliJ loads the ShadowJar plugin
+  // This should be replaced with the following line if it is fixed
+  shadowJar {
+//    named<ShadowJar>("shadowJar", ShadowJar::class) {
+    manifest {
+      attributes(
+        "paperweight-mappings-namespace" to "mojang"
+      )
+    }
+  }
+
+  test {
+    useJUnitPlatform()
+    jvmArgs(
+      "-XX:+EnableDynamicAgentLoading"
+    )
+  }
+
+}
+
 
 val targetJavaVersion = 21
 
 java {
-    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
+val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+sourceCompatibility = javaVersion
+targetCompatibility = javaVersion
 
-    if (JavaVersion.current() < javaVersion) {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
-    }
+if (JavaVersion.current() < javaVersion) {
+toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+}
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
+options.encoding = "UTF-8"
 
-    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
-        options.release.set(targetJavaVersion)
-    }
+if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+options.release.set(targetJavaVersion)
+}
 }
 
 tasks.processResources {
-    // dynamic for the plugin.json
-    val props = mapOf(
-        "version" to version,
-        "name" to project.property("name") as String,
-        "description" to project.property("description") as String,
-        "website" to project.property("website") as String,
-        "authors" to (project.property("authors") as String).trim('[', ']').split(","),
-        "contributors" to (project.property("contributors") as String).trim('[', ']').split(","),
-        "main" to project.property("main") as String,
-        "prefix" to project.property("prefix") as String,
-        "api_version" to targetMcVersion,
-    )
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
+// dynamic for the plugin.json
+val props = mapOf(
+"version" to version,
+"name" to project.property("name") as String,
+"description" to project.property("description") as String,
+"website" to project.property("website") as String,
+"authors" to (project.property("authors") as String).trim('[', ']').split(","),
+"contributors" to (project.property("contributors") as String).trim('[', ']').split(","),
+"main" to project.property("main") as String,
+"prefix" to project.property("prefix") as String,
+"api_version" to targetMcVersion,
+)
+inputs.properties(props)
+filteringCharset = "UTF-8"
 
-    filesMatching("plugin.yml") {
-        expand(props)
-    }
+filesMatching("plugin.yml") {
+expand(props)
+}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "Szlaban"
+
+// add paper's repo to plugins
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    maven("https://papermc.io/repo/repository/maven-public/")
+  }
+}
